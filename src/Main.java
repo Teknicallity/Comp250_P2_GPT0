86db3d69e39bb5c047b091093560654037f2ad1f
Teknicallity <joshua.s.3322@gmail.com>
2023-08-05 11:18:53 -0400

    Step 3 finished Read into list and print out list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
+    static KeyWordList keyWordList = new KeyWordList();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length > 0){
+            intake(args[0]);
+        } else {
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Enter filename: ");
+            String file = keyboard.nextLine();
+            intake(file);
+        }
+
+        keyWordList.print();
+    }
+
+    public static void intake(String filename){
+        try {
+            Scanner reader = new Scanner(new File(filename));
+            String nextWord;
+            while (reader.hasNext()){
+                nextWord = filter(reader.next());
+                if (!nextWord.equals("") || !nextWord.equals(" ") || !nextWord.equals(null)){
+                    keyWordList.add(nextWord);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static String filter(String word){
+        StringBuilder modifiedStr = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.toLowerCase().charAt(i);
+            if (c >= 'a' && c <= 'z' )
+                modifiedStr.append(c);
+        }
+        return modifiedStr.toString();
     }
 }
